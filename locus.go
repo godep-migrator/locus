6c0645c51b628d90b97b0a1ae382ef3b7c062513
Alan Harris <ah.anachronistic@gmail.com>
2014-05-25 15:18:55 -0400

    Add supporting code to check whether or not the point is contained.
    Relies on the gogeos lib, which wraps geos.

--- locus.go
+++ locus.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"github.com/paulsmith/gogeos/geos"
+	"strconv"
+)
+
 func main() {
 	serveHttp()
+}
+
+func testCoordinatePresence(x, y, polygyon string) bool {
+	fx, err := strconv.ParseFloat(x, 64)
+	if err != nil {
+		return false
+	}
+
+	fy, err := strconv.ParseFloat(y, 64)
+	if err != nil {
+		return false
+	}
+
+	point, err := geos.NewPoint(geos.NewCoord(fx, fy))
+	if err != nil {
+		return false
+	}
+
+	boundary, err := geos.FromWKT(polygyon)
+	if err != nil {
+		return false
+	}
+
+	result, err := boundary.Contains(point)
+	if err != nil {
+		return false
+	}
+
+	return result
 }
--- web.go
+++ web.go
@@ -4,8 +4,10 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func serveHttp() {
@@ -14,6 +16,7 @@ func serveHttp() {
 
 	// UI Calls
 	m.Get("/", homeHandler)
+	m.Post("/", testCoordinatesHandler)
 	m.NotFound(notFoundHandler)
 
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), m); err != nil {
@@ -29,4 +32,18 @@ func homeHandler(w http.ResponseWriter, req *http.Request) {
 func notFoundHandler(w http.ResponseWriter, req *http.Request) {
 	t := template.Must(template.New("404").ParseFiles("views/layout.html", "views/404.html"))
 	t.ExecuteTemplate(w, "layout", "Whoops!")
+}
+
+func testCoordinatesHandler(req *http.Request, r render.Render) {
+	req.ParseForm()
+	x := req.FormValue("point-x")
+	y := req.FormValue("point-y")
+	polygon := req.FormValue("polygon-wkt")
+	contained := testCoordinatePresence(x, y, polygon)
+
+	r.JSON(http.StatusOK,
+		map[string]interface{}{
+			"contained": contained,
+			"timestamp": time.Now().UTC().Unix(),
+		})
 }
